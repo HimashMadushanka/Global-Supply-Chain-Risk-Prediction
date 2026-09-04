f91808e55417e2fec1db201acbccdc50cc51c502
Himash Madushanka <himashmadushanka975@gmail.com>
2026-09-04 23:37:01 +0530

    add

--- run_flask.py
+++ run_flask.py
@@ -8,7 +8,7 @@ from flask_app.app import app
 
 if __name__ == "__main__":
     print("=" * 70)
-    print("🚀 Starting Supply Chain Disruption Predictor Flask App")
-    print("🌐 Dashboard URL: http://localhost:5000")
+    print(">> Starting Supply Chain Disruption Predictor Flask App")
+    print(">> Dashboard URL: http://localhost:5000")
     print("=" * 70)
     app.run(host="127.0.0.1", port=5000, debug=True)
